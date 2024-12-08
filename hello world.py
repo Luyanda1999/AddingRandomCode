04b1008f5ec59a3b339d64bcca52021e5f155243
Luyanda Ndwandwe <76037849+Luyanda1999@users.noreply.github.com>
2024-12-08 07:00:03 +0200

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,16 @@
+print("Hello, World")
+a = "2"
+b= "2"
+
+c=a+b
+print(c)
+
+numbers= [1,2,3,4,5]
+i=0
+#for
+for items in numbers:
+    print(items)
+#while
+while i<len(numbers):
+    print(numbers[i])
+    i+=1
