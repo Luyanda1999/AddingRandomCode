137405f84bd8bb0edeafd99cf870b868e5555f91
Luyanda Ndwandwe <76037849+Luyanda1999@users.noreply.github.com>
2024-12-08 15:24:37 +0200

    Add files via upload

--- weight.py
+++ weight.py
@@ -1,11 +1,16 @@
-weight = input("Weight: ")
-unit= input("(K)gs or (L)bs: ")
+try:
+    weight = input("Weight: ")
+    unit= input("(K)gs or (L)bs: ")
 
-if unit == "K" or unit == "k":
-    size = float(weight)*0.454
-    print("Weight is " + str(size) + "in Kgs")
-elif unit == "L" or unit == "l":
-    size = float(weight)
-    print("Weight is " + str(weight) + " in Pounds")
-else:
-    print('Could not computer')
+    if unit == "K" or unit == "k":
+        size = float(weight)*0.454
+        print("Weight is " + str(size) + "in Kgs")
+    elif unit == "L" or unit == "l":
+        size = float(weight)
+        print("Weight is " + str(weight) + " in Pounds")
+    elif unit != "L" or unit !="l" or unit != "K" or unit !="k":
+        print("Can not compute unit type please use 'l' or 'k'")
+except ValueError:
+    print("Please use number when typing the weight")
+except Exception:
+    print("Something is not right")
