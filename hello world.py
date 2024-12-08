66f9d5a348d26eda84c35433e9603bd69ea11418
Luyanda Ndwandwe <76037849+Luyanda1999@users.noreply.github.com>
2024-12-08 08:02:59 +0200

    Add files via upload

--- hello world.py
+++ hello world.py
@@ -14,3 +14,22 @@ for items in numbers:
 while i<len(numbers):
     print(numbers[i])
     i+=1
+
+#Hospital: this code house different variables
+name = "Luyanda"
+print("Hello, " + name)
+name = input("What is your name? ")
+age = 35
+In_Hospital= True
+
+print("Name of the Patient is: " + name)
+print("Age of the Patient: " + str(age))
+print("Is the Patient in the Hospital: " + str(In_Hospital))
+
+#sum of 2 numbers
+First_Number = input("First number: ") #initial inputs can only take strings and hence why we convert them to number int or float
+Second_NUmber = input("Second Number: ")
+
+Total_Sum = float(First_Number) + float(Second_NUmber)
+print(Total_Sum)#this is a  number
+print("The total sum of 2 Number is: " + str(Total_Sum))#this is a string
