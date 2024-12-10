cc40fd5413a0c2a182670a7eac33eb8a511d5b06
Luyanda Ndwandwe <76037849+Luyanda1999@users.noreply.github.com>
2024-12-10 07:45:30 +0200

    Add files via upload

---
+++ making an F.py
@@ -0,0 +1,10 @@
+numbers =[5,2,5,2,2]
+for i in numbers:
+    print(i*"#")
+
+numbering =[5,2,5,2,2]
+for i in numbering:
+    f_count=""
+    for x in range(i):
+        f_count += "x"
+    print(f_count)
---
+++ price game.py
@@ -0,0 +1,10 @@
+prices = [10,20,30]
+sum = 0
+for i in prices:
+    sum += i
+print(sum)
+
+#nested loops
+for x in range(5):
+    for y in range(4):
+        print(f'Co-ordinates: ({x},{y})')
