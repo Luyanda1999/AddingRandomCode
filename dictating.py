fc9317d2ac9c625bf5923c576d224602c9415d27
Luyanda Ndwandwe <76037849+Luyanda1999@users.noreply.github.com>
2024-12-15 22:44:52 +0200

    Add files via upload

---
+++ dictating.py
@@ -0,0 +1,10 @@
+customer = {
+    "name" : "Luyanda Ndwandwe",
+    "age" : "25",
+    "blood-type" : "O+"
+}
+if customer["blood-type"] == "O+":
+    print(customer["name"])
+    print(customer["age"])
+else:
+    print("Nothing here! ")
