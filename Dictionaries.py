a63c675a0b644377bafd58d5f6c7a429c3a0e168
Luyanda Ndwandwe <76037849+Luyanda1999@users.noreply.github.com>
2024-12-15 14:25:16 +0200

    Create Dictionaries.py

---
+++ Dictionaries.py
@@ -0,0 +1,17 @@
+numbers={
+    "1" : "One",
+    "2" : "Two",
+    "3" : "Three",
+    "4" : "Four",
+    "5" : "Five",
+    "6" : "Six",
+    "7" : "Seven",
+    "8" : "Eight",
+    "9" : "Nine",
+    "0" : "Zero"
+}
+output=""
+phone = input("Phone: ")
+for i in phone:
+    output += numbers.get(i,"!")+" "
+print(output)
