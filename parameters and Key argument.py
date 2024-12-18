72d65c34a01199c213daad4218102507699ad846
Luyanda Ndwandwe <76037849+Luyanda1999@users.noreply.github.com>
2024-12-18 07:52:46 +0200

    Add files via upload

---
+++ parameters and Key argument.py
@@ -0,0 +1,9 @@
+#parameters and Key argument
+def greet_user(name, surname):#arguements will not run when both values have not been placed in the program
+    print(f"Hello {name} {surname}")
+    print("welcome aboard")
+
+
+print("Start text")
+greet_user("Luyanda", "Ndwandwe")
+print("End text")
