66555eca09408dd79ee65a12738d7a0893227eb0
Luyanda Ndwandwe <76037849+Luyanda1999@users.noreply.github.com>
2024-12-23 22:56:30 +0200

    Add files via upload

---
+++ Modules trial/converters.py
@@ -0,0 +1,5 @@
+import Weight_function
+
+print(Weight_function.kgs_to_lbs(70))
+
+print(Weight_function.lbs_to_kgs(155))
---
+++ Modules trial/converters2.py
@@ -0,0 +1,3 @@
+from Weight_function import kgs_to_lbs
+
+print(kgs_to_lbs(50))
