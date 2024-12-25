b81a3f8072d5115b1c57cf2910955cebd423039a
Luyanda Ndwandwe <76037849+Luyanda1999@users.noreply.github.com>
2024-12-25 12:25:57 +0200

    Add files via upload

---
+++ Modules trial/app.py
@@ -0,0 +1,20 @@
+#packages code
+######################################################
+import ecommerce.shipping
+
+ecommerce.shipping.calc_shipping()
+######################################################
+
+from ecommerce import shipping
+
+shipping.calc_shipping()
+######################################################
+#another example is:
+######################################################
+
+from ecommerce.shipping import calc_shipping
+
+calc_shipping()
+######################################################
+
+
