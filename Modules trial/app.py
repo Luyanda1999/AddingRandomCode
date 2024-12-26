e5968eacc6b91e4eaacbe84d6065793f307b67db
Luyanda Ndwandwe <76037849+Luyanda1999@users.noreply.github.com>
2024-12-26 06:51:13 +0200

    Update app.py

--- Modules trial/app.py
+++ Modules trial/app.py
@@ -21,4 +21,15 @@ calc_shipping()
 
 ######################################################
 
+import random # randon generator comes addition with a python extension
 
+for i in range(1):
+    print(random.randint(1,6))#rand it pick a random number that has been generated from the rage.
+
+#Random choice
+#import random #random generator already imported
+
+brothers= ["Glen","Joko","Trinco","Zolani"]
+
+Rand_Bro= random.choice(brothers)
+print(Rand_Bro)
