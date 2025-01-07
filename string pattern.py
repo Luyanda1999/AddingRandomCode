2db499d0b818dd7057cc74062c61b5722e62369d
Luyanda Ndwandwe <76037849+Luyanda1999@users.noreply.github.com>
2025-01-07 09:05:14 +0200

    Add files via upload

---
+++ string pattern.py
@@ -0,0 +1,17 @@
+my_string = "python"
+x=0
+
+for i in my_string:
+    x +=1
+    print(my_string[0:x])
+for i in my_string:
+    x-=1
+    print(my_string[0:x])
+
+#def myString(self):
+#    for i in my_string:
+#        x +=1
+#        print(my_string[0:x])
+#    for i in my_string:
+#        x-=1
+#        print(my_string[0:x])
