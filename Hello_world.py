5a198b3f6a2daa2dd3938c9f582d85ac8e17cbd5
Luyanda Ndwandwe <76037849+Luyanda1999@users.noreply.github.com>
2026-02-20 07:47:31 +0200

    Update Hello_world.py

--- Hello_world.py
+++ Hello_world.py
@@ -1,6 +1,6 @@
 print('Hello, World')
-a = int(input(Enter the first number: ))
-b = int(input(Enter the second number: ))
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
 
 c = a + b
 print(c)
